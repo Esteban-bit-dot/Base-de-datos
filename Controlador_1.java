3021510f72804134afe328583ea2590637bd170b
Esteban-bit-dot <eandrescardales@gmail.com>
2025-04-09 08:43:20 -0500

    Create Controlador_1.java

---
+++ Controlador_1.java
@@ -0,0 +1,17 @@
+package Tienda_carros;
+
+import Tienda_carros.Usuario;
+import Tienda_carros.Formulario_Registro_Usuario;
+
+public class Controlador_1{
+    
+    private Usuario objUsuario;
+    private Formulario_Registro_Usuario objformulario;
+    
+    
+    public void Controlador(){
+     this.objUsuario = new Usuario();
+     this.objformulario = new Formulario_Registro_Usuario();
+    
+ }
+}
