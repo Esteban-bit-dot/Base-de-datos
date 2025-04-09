f8a67f994ef72048f9574cfbb163148ef3f3f217
Esteban-bit-dot <eandrescardales@gmail.com>
2025-04-09 08:42:25 -0500

    Create Usuario.java

---
+++ Usuario.java
@@ -0,0 +1,31 @@
+package Tienda_carros;
+
+
+public class Usuario {
+
+   private String Nombre_usuario;
+    private String Correo_usuario;
+    private String Contraseña_usuario;
+    
+    public void Vehiculo(){
+        this.Nombre_usuario;
+        this.Correo_usuario;
+        this.Contraseña_usuario;
+    }
+
+    public String getNombre_usuario() {
+        return Nombre_usuario;
+    }
+
+    public void setSerial_vehiculo(String Nombre_usuario) {
+        this.Nombre_usuario = Nombre_usuario;
+    }
+
+    public String getCorreo_usuario() {
+        return Correo_usuario;
+    }
+
+    public void setCorreo_usuario(String Correo_usuario) {
+        this.Correo_usuario = Correo_usuario;
+    }
+    
