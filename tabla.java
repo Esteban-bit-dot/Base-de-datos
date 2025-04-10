87d88ae70972c096654b16a1a6499ac4866d0c3c
Esteban-bit-dot <eandrescardales@gmail.com>
2025-04-10 16:46:44 -0500

    Create tabla.java

---
+++ tabla.java
@@ -0,0 +1,66 @@
+package vista;
+
+
+public class tablaRegistro_Usuario extends javax.swing.JPanel {
+    private String Nombre_usuario;
+    private String Correo_usuario;
+    private String Contraseña_usuario;
+   
+    public tablaRegistro_Usuario() {
+        initComponents();
+    }
+    
+   
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+
+        jTable1.setBackground(new java.awt.Color(255, 51, 0));
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null}
+            },
+            new String [] {
+                "Nombre", "Correo", "Contraseña"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(19, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(19, Short.MAX_VALUE))
+        );
+    }// </editor-fold>                        
+     public void  UsuariosRegistrados(){
+    String Nombre_Usuario = this.Nombre_usuario;
+    String Correo_Usuario = this.Correo_usuario;
+    String Contraseña_Usuario = this.Contraseña_usuario;
+    
+    String Columun1 = this.Nombre_usuario;
+    String Column2 = this.Correo_usuario;
+    String Column3 = this.Contraseña_usuario;
+}
+
+    // Variables declaration - do not modify                     
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
+    // End of variables declaration                   
+}
