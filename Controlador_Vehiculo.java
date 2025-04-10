4418f7e21c5bcec36b8b654f6beda1451a4677be
Esteban-bit-dot <eandrescardales@gmail.com>
2025-04-10 16:40:31 -0500

    Update Controlador_Vehiculo.java

--- Controlador_Vehiculo.java
+++ Controlador_Vehiculo.java
@@ -1,17 +1,15 @@
-package Tienda_carros;
+package controlador;
 
- import modelo.Vehiculo;
+import modelo.Vehiculo;
+import vista.Formulario_Registro_Vehiculo;
 
-import Tienda_carros.Vista.Formulario_Registro_Vehiculo;
-
-public class Controlador_Vehiculo {
-   
+public class Controlador{
     
     private Vehiculo objVehiculo;
     private Formulario_Registro_Vehiculo objformulario;
     
     
-    public void Controlador(){
+    public Controlador(){
      this.objVehiculo = new Vehiculo();
      this.objformulario = new Formulario_Registro_Vehiculo();
     
